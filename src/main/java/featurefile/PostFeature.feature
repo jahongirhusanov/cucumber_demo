014feee8dc89a6bb63561f7e3be9b32a43f1cf82
jahongirhusanov <husanov.y.jahongir@gmail.com>
2022-11-01 13:22:12 -0500

    initial setUp

---
+++ src/main/java/featurefile/PostFeature.feature
@@ -0,0 +1,10 @@
+Feature: Post feature of Facebook
+
+  Scenario: Post a text on user wall
+    Given User should be logged in
+    And should be present at its own wall
+    When I type the text in the test box
+    And Click on Post button
+    Then The message should be get posted
+    Then ajshd sjdhsjd jhds
+
---
+++ src/main/java/stepdfn/PostStepDfn.java
@@ -0,0 +1,35 @@
+package stepdfn;
+
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class PostStepDfn {
+    @Given("^User should be logged in$")
+    public void user_should_be_logged_in() throws Throwable {
+        throw new PendingException();
+    }
+
+    @When("^I type the text in the test box$")
+    public void i_type_the_text_in_the_test_box() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Then("^The message should be get posted$")
+    public void the_message_should_be_get_posted() throws Throwable {
+        throw new PendingException();
+    }
+
+    @And("^should be present at its own wall$")
+    public void should_be_present_at_its_own_wall() throws Throwable {
+        throw new PendingException();
+    }
+
+    @And("^Click on Post button$")
+    public void click_on_post_button() throws Throwable {
+        throw new PendingException();
+    }
+
+}
